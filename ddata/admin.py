555fe94bf14e155c3764e54be5388869cc651c2d
tessazglue <tessavoon@Tessas-MacBook-Pro.local>
2017-01-20 15:19:31 -0800

    first commit

---
+++ ddata/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Trip, RideDetails
+
+admin.site.register(Trip)
+admin.site.register(RideDetails)
+# Register your models here.
---
+++ ddata/models.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Trip(models.Model):
+    pickup = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
+    start_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    start_lon = models.DecimalField(max_digits=9, decimal_places=6)
+    end_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    end_lon = models.DecimalField(max_digits=9, decimal_places=6)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.pickup + " - " + self.destination
+
+class RideDetails(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    date = models.CharField(max_length=200)
+    time = models.CharField(max_length=200)
+    distance = models.FloatField(default=0)
+    duration = models.IntegerField(default=0)
+    high_est = models.IntegerField(default=0)
+    low_est = models.IntegerField(default=0)
+    estimate = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.estimate
